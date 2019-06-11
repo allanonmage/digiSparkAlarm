bdfd6700016d9091f667a3d2a9f0f4b2eecae1b7
allanonmage <allanonmage@gmail.com>
2019-06-11 13:50:54 -0400

    Add files via upload

---
+++ digispark_alarm.ino
@@ -0,0 +1,26 @@
+void setup(){
+  pinMode(1,INPUT); //detects circuit
+  pinMode(4,OUTPUT); // LED for alarm
+  pinMode(5,OUTPUT); // siren
+  
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  
+  
+} // end setup()
+
+
+
+void loop(){
+  
+  if(digitalRead(1)==LOW){
+    digitalWrite(4,HIGH);
+    digitalWrite(5,HIGH);
+  } // end if
+  
+  if(digitalRead(1)==HIGH){
+    digitalWrite(4,LOW);
+    digitalWrite(5,LOW);
+  } // end if
+  
+} // end loop()
